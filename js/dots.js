87d62871f6d6b50933ce93caab4be8ee579f2c02
mnvijay <hsmn.vijay@gmail.com>
2017-01-02 13:11:22 +0530

    WPU: Transition Update.

--- js/dots.js
+++ js/dots.js
@@ -10,6 +10,51 @@ $(document).ready(function(){ // wait for document ready
         'slow');
 });
 
+    function greedyJumbotron() {
+    var HEIGHT_CHANGE_TOLERANCE = 300; // Approximately URL bar height in Chrome on tablet
+
+    var jumbotron = $(this);
+    var viewportHeight = $(window).height();
+
+    $(window).resize(function () {
+        if (Math.abs(viewportHeight - $(window).height()) > HEIGHT_CHANGE_TOLERANCE) {
+            viewportHeight = $(window).height();
+            update();
+        }
+    });
+
+    function update() {
+        jumbotron.css('height', viewportHeight + 'px');
+        jumbatron.css('background-position', '0vh 85.31vh, ');
+    }
+
+    update();
+}
+
+$('.wrap').each(greedyJumbotron);
+
+    function waves() {
+    var HEIGHT_CHANGE_TOLERANCE = 300; // Approximately URL bar height in Chrome on tablet
+
+    var jumbotron = $(this);
+    var viewportHeight = $(window).height();
+
+    $(window).resize(function () {
+        if (Math.abs(viewportHeight - $(window).height()) > HEIGHT_CHANGE_TOLERANCE) {
+            viewportHeight = $(window).height();
+            update();
+        }
+    });
+
+    function update() {
+        jumbotron.css('height', viewportHeight + 'px');
+        jumbotron.css({backgroundPosition: '0vh 85.31vh -100px 0px'});
+    }
+
+    update();
+}
+
+$('#m_home').each(waves);
 
     
     
